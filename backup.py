5f2891f281302c4198b77b95f84f5ea5f36c5631
NadinDesyatova <nadindinina@yandex.ru>
2023-11-14 21:09:58 +0300

    corrected files: backup.py and requirements.txt

--- backup.py
+++ backup.py
@@ -7,9 +7,8 @@ from urllib.parse import urlencode
 class VKAPIClient:
     base_url_API = 'https://api.vk.com/method'
 
-    def __init__(self, token, user_id):
+    def __init__(self, token):
         self.token = token
-        self.user_id = user_id
 
     def get_common_params(self):
         return {
@@ -17,10 +16,10 @@ class VKAPIClient:
             'v': '5.131'
         }
 
-    def get_profile_photos(self):
+    def get_profile_photos(self, user_id):
         params = self.get_common_params()
         params.update({
-            'owner_id': self.user_id, 
+            'owner_id': user_id, 
             'album_id': 'profile', 
             'rev': '1',
             'extended': '1',
@@ -31,8 +30,8 @@ class VKAPIClient:
 
         return response.json()
 
-    def get_photos_information(self):      
-        photos_information = self.get_profile_photos().get('response', {}).get('items', {})
+    def __get_photos_information(self, user_id):      
+        photos_information = self.get_profile_photos(user_id).get('response', {}).get('items', {})
         photos_list_for_save = []
 
         for photo_info in photos_information:
@@ -65,8 +64,8 @@ class VKAPIClient:
             'photos_information_for_file': photos_information_for_file
         }
 
-    def saving_local_photos(self):
-        photos_information = self.get_photos_information().get('photos_list_for_save')
+    def saving_local_photos(self, user_id):
+        photos_information = self.__get_photos_information(user_id).get('photos_list_for_save')
 
         for photo in photos_information:
             file_name = f"{photo['name']}.jpg"
@@ -75,11 +74,15 @@ class VKAPIClient:
             with open(file_name, 'wb') as f:
                 f.write(photo_content.content)
         
-    def saving_photos_information_file(self):
-        dict = {"data": self.get_photos_information().get('photos_information_for_file')}
+    def saving_photos_information_file(self, file_name, user_id):
+        dict = {"data": self.__get_photos_information(user_id).get('photos_information_for_file')}
 
-        with open('photos_information.json', 'w') as f:            
+        json_file_name = f'{file_name}.json'
+
+        with open(json_file_name, 'w') as f:            
             json.dump(dict, f, indent = 2)
+        
+        return dict['data']
 
 
 class YADiskAPIClient:
@@ -88,29 +91,24 @@ class YADiskAPIClient:
     def __init__(self, token):
         self.token = token
     
-    def get_headers(self):
+    def __get_headers(self):
         return {'Authorization': self.token}
     
-    def creating_folder(self):
-        params = {'path': 'Photos_from_VK'}
+    def __creating_folder(self, folder_name):
+        params = {'path': f'{folder_name}'}
         requests.put(self.base_url_ya_disk,
                     params=params,
-                    headers=self.get_headers())
+                    headers=self.__get_headers())
 
-    def uploading_photos_to_disk(self):
-        with open('photos_information.json', encoding='utf-8') as f:
-            data_json = json.load(f)
-        
-        photos_information  = data_json['data']
-
-        self.creating_folder()
+    def uploading_photos_to_disk(self, folder_name, photos_information):
+        self.__creating_folder(folder_name)
 
         for photo in photos_information:
-            params = {'path': f"Photos_from_VK/{photo['file_name']}"}
+            params = {'path': f"{folder_name}/{photo['file_name']}"}
 
             response_1 = requests.get(f'{self.base_url_ya_disk}/upload', 
                         params=params,
-                        headers=self.get_headers())
+                        headers=self.__get_headers())
         
             url_for_upload = response_1.json().get('href')
             file_name = photo['file_name']
@@ -127,3 +125,19 @@ logging.info("An INFO")
 logging.warning("A WARNING")
 logging.error("An ERROR")
 logging.critical("A message of CRITICAL severity")
+
+
+if __name__ == '__main__':
+    TOKEN_VK = input('Введите токен ВК: ')
+    token_ya = input('Введите токен с Полигона Яндекс.Диска.: ')
+    current_user_id = input('Введите числовой id пользователя ВК: ')
+
+    vk_client = VKAPIClient(TOKEN_VK)
+    vk_client.get_profile_photos(current_user_id)
+
+    vk_client.saving_local_photos(current_user_id)
+    data = vk_client.saving_photos_information_file('photos_information', current_user_id)
+
+
+    ya_client = YADiskAPIClient(token_ya)
+    ya_client.uploading_photos_to_disk('Photos_from_VK', data)
